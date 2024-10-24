fe6f6238237107011a309f67aac2b0176f6ac8e0
sdeix <you@example.com>
2024-10-24 22:59:10 +0700

    начало

--- html/index.php
+++ html/index.php
@@ -1,1 +1,29 @@
-<h1>hello</h1>
+<?php
+class Cat
+{
+    private $name;
+    
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function sayHello()
+    {
+        echo 'Привет! Меня зовут ' . $this->name . '.';
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
+
+$cat1 = new Cat('Снежок');
+echo $cat1->getName();

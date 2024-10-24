979ff241b0c8977780190ba9ac884a57830ae63f
sdeix <you@example.com>
2024-10-24 23:07:02 +0700

    done

--- html/index.php
+++ html/index.php
@@ -2,11 +2,12 @@
 class Cat
 {
     private $name;
-    
+    private $color;
 
-    public function __construct(string $name)
+    public function __construct(string $name, string $color)
     {
         $this->name = $name;
+        $this->color = $color;
     }
 
     public function sayHello()
@@ -23,7 +24,18 @@ class Cat
     {
         return $this->name;
     }
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+    public function getInfo()
+    {
+        echo 'Привет! Меня зовут: '. $this->name.' цвет:'.$this->color."<br>";
+    }
 }
 
-$cat1 = new Cat('Снежок');
-echo $cat1->getName();
+$cat1 = new Cat('Снежок',' Белый');
+$cat2 = new Cat('Барсик',' Рыжий');
+
+$cat1->getInfo();
+$cat2->getInfo();
